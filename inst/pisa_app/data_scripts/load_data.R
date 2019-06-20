793543142d00f5cfabf60b47d540938d175b5f44
aspenh1 <aspenhammond@yahoo.com>
2019-06-20 08:49:22 +0200

    Update load_data.R

--- inst/pisa_app/data_scripts/load_data.R
+++ inst/pisa_app/data_scripts/load_data.R
@@ -11,6 +11,9 @@ cred <- readRDS("./data_scripts/token.RDS")
 username <- cred$username
 password <- cred$password
 
+# username <- 'preview'#cred$username
+# password <- 'preview' #cred$password
+
 ## call web service for current year
 call <- paste0("http://apps.who.int/gho/athena/flumart/MEASURE/IMPACT,IMPACT_CL,IMPACT_COM,TRANSMISSION,TRANSMISSION_CL,TRANSMISSION_COM,SERIOUSNESS,SERIOUSNESS_CL,SERIOUSNESS_COM?filter=PISA/YEAR:",
                year_current,
@@ -37,7 +40,7 @@ get_json <- fromJSON(get_text)
 df <- rbind(df, get_json)
 
 ## call web service for two years prior
-# call <- paste0("http://apps.who.int/gho/athena/flumart/MEASURE/IMPACT,IMPACT_CL,IMPACT_COM,TRANSMISSION,TRANSMISSION_CL,TRANSMISSION_COM,SERIOUSNESS,SERIOUSNESS_CL,SERIOUSNESS_COM?filter=YEAR:",
+# call <- paste0("http://apps.who.int/gho/athena/flumart/MEASURE/IMPACT,IMPACT_CL,IMPACT_COM,TRANSMISSION,TRANSMISSION_CL,TRANSMISSION_COM,SERIOUSNESS,SERIOUSNESS_CL,SERIOUSNESS_COM?filter=PISA/YEAR:",
 #                year_prev_2,
 #                "&format=json&profile=pisa")
 # get_data <- GET(call, authenticate(username, password, type = "basic"))
@@ -62,14 +65,26 @@ df$ISO2 <- ifelse(df$COUNTRY_TITLE == "United Kingdom", "GB", df$ISO2)
 df$TRANSMISSION[nchar(df$TRANSMISSION) == 0] <- "Not Available"
 df$TRANSMISSION_CL[nchar(df$TRANSMISSION_CL) == 0] <- "Not Available"
 df$TRANSMISSION_COM[nchar(df$TRANSMISSION_COM) == 0] <- "Not Available"
+########Changes into the sentence cases############
+df$TRANSMISSION=gsub("([[:alpha:]])([[:alpha:]]+)", "\\U\\1\\L\\2", df$TRANSMISSION, perl=TRUE)
+df$TRANSMISSION_CL=gsub("([[:alpha:]])([[:alpha:]]+)", "\\U\\1\\L\\2", df$TRANSMISSION_CL, perl=TRUE)
+##########
 
 df$SERIOUSNESS[nchar(df$SERIOUSNESS) == 0] <- "Not Available"
 df$SERIOUSNESS_CL[nchar(df$SERIOUSNESS_CL) == 0] <- "Not Available"
 df$SERIOUSNESS_COM[nchar(df$SERIOUSNESS_COM) == 0] <- "Not Available"
+########Changes into the sentence cases############
+df$SERIOUSNESS=gsub("([[:alpha:]])([[:alpha:]]+)", "\\U\\1\\L\\2", df$SERIOUSNESS, perl=TRUE)
+df$SERIOUSNESS_CL=gsub("([[:alpha:]])([[:alpha:]]+)", "\\U\\1\\L\\2", df$SERIOUSNESS_CL, perl=TRUE)
+##########
 
 df$IMPACT[nchar(df$IMPACT) == 0] <- "Not Available"
 df$IMPACT_CL[nchar(df$IMPACT_CL) == 0] <- "Not Available"
 df$IMPACT_COM[nchar(df$IMPACT_COM) == 0] <- "Not Available"
+########Changes into the sentence cases############
+df$IMPACT=gsub("([[:alpha:]])([[:alpha:]]+)", "\\U\\1\\L\\2", df$IMPACT, perl=TRUE)
+df$IMPACT_CL=gsub("([[:alpha:]])([[:alpha:]]+)", "\\U\\1\\L\\2", df$IMPACT_CL, perl=TRUE)
+##########
 
 ## create UI data
 year_ui <- sort(unique(df$ISO_YEAR[nchar(df$ISO_YEAR) == 4])) #Incorrect Year in data
